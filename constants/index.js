93788a14ab5a809dac8ce404e000ffdf28650899
chango <kimtothechang@gmail.com>
2022-11-30 23:07:28 +0900

    add: Table명 배열 추가

--- constants/index.js
+++ constants/index.js
@@ -1,3 +1,4 @@
 const JSON_URL = 'https://raw.githubusercontent.com/capedbaldy82/websql-test/main/assets';
+const TABLE_ARRAY = ['student', 'professor'];
 
-export { JSON_URL };
+export { JSON_URL, TABLE_ARRAY };
